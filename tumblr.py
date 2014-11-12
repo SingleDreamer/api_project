0aec652889d3fb967a971384fcd538fbeab6d1bc
Cindy <cindw@hotmail.com>
2014-11-11 19:29:46 -0500

    title/image/text

--- tumblr.py
+++ tumblr.py
@@ -6,41 +6,95 @@ random.seed()
 
 @app.route("/")
 def index():
-        tag = request.args.get("text")
+        title = request.args.get("title")
+        image = request.args.get("image")
+        text = request.args.get("text")
         submit = request.args.get("submit")
-        if (submit=="Submit" and tag != ""):
-                print "yes"
-                return redirect("/t/"+tag)
-	return render_template("home.html")
+        if (submit=="Submit" and title != "" and image != "" and text != ""):
+                return redirect("/randomize/"+title+"&"+image+"&"+text)
+        return render_template("home.html")
 
-@app.route("/t/<tag>")
-def tag(tag=""):
-	url="http://api.tumblr.com/v2/tagged?tag=%s&api_key=aTQcvkZkpZbz7ILTsi8ekrUFE0maSPweft6mM1yyJhQBdnV5eb"
-	url = url%(tag)
-	request = urllib2.urlopen(url)
-	resultstring = request.read()
-	result = json.loads(resultstring)
-	s = ""
-	#for item in result['response']:
- 		#print item
+@app.route("/randomize/<title>&<image>&<text>")
+def tag(title="",image="",text=""):
+        t = ""
+        b = ""
+
+        #---Title---
+        url="http://api.tumblr.com/v2/tagged?tag=%s&api_key=aTQcvkZkpZbz7ILTsi8ekrUFE0maSPweft6mM1yyJhQBdnV5eb"
+        url = url%(title)
         try:
-                r = random.randint(0, len(result['response'])-1)
-                print r
-                item = result['response'][r]
-                s= s + "<img src=%s>"%(item['photos'][0]['original_size']['url'])
-                #print s
+                request = urllib2.urlopen(url)
+                resultstring = request.read()
+                result = json.loads(resultstring)
+                while (t==""):
+                        try:
+                                r = random.randint(0, len(result['response'])-1)
+                                #                        print r
+                                item = result['response'][r]
+                                t = t + item['title']
+                        except:
+                                pass
         except:
                 pass
+        #---Title---
 
-        if (s==""):
+        #---Image---
+        url="http://api.tumblr.com/v2/tagged?tag=%s&api_key=aTQcvkZkpZbz7ILTsi8ekrUFE0maSPweft6mM1yyJhQBdnV5eb"
+        url = url%(image)
+        try:
+                request = urllib2.urlopen(url)
+                resultstring = request.read()
+                result = json.loads(resultstring)
+
+                while (b==""):
+                        try:
+                                r = random.randint(0, len(result['response'])-1)
+                                #                        print r
+                                item = result['response'][r]
+                                #                        print item
+                                b = b + "<img src=%s>"%(item['photos'][0]['original_size']['url'])
+                                #                print b
+                        except:
+                                pass
+        except:
+                pass
+        #---Image---
+
+        tx="" 
+        #---Text---
+        url="http://api.tumblr.com/v2/tagged?tag=%s&api_key=aTQcvkZkpZbz7ILTsi8ekrUFE0maSPweft6mM1yyJhQBdnV5eb"
+        url = url%(text)
+                
+        try:
+                request = urllib2.urlopen(url)
+                resultstring = request.read()
+                result = json.loads(resultstring)
+                res = result['response']
+
+                while (tx==""):
+                        try:
+                                r = random.randint(0, len(res)-1)
+                                item = res[r]
+                                #                                print r
+                                #                                print item
+                                tx += tx + "<br>"+item['body']
+                        except:
+                                pass
+        except:
+                pass
+                         
+        b+=tx
+        
+        if (b==""):
                 return redirect("/error")
+                #---Text---
 
-	return render_template("tag.html",body=s)
-
+        return render_template("tag.html",title=t,body=b)
+        
 @app.route("/error")
 def error():
         return render_template("error.html")
 
 if __name__=="__main__":
-   app.debug=True
-   app.run(host="0.0.0.0",port=8000)
+        app.debug=True
+        app.run(host="0.0.0.0",port=8000)
